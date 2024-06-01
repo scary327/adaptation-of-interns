250233b7d43467901916cb8247a340d33cf92799
scary327 <zef1rflis@yandex.ru>
2024-06-01 05:07:00 +0500

    fix vite config #1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 80,
+    host: true
+  },
+  server: {
+    port: 80,
+    host: true
+  }
 })
