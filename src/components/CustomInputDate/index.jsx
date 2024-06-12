78f5b056f0ab9b9c091b4da603b2986f7585876c
scary327 <zef1rflis@yandex.ru>
2024-06-13 00:08:37 +0500

    plans not my, they 'us'

--- src/components/CustomInputDate/index.jsx
+++ src/components/CustomInputDate/index.jsx
@@ -2,12 +2,13 @@ import styles from './input.module.css';
 
 export const InputDate = (props) => {
 
-    const { onChange, min } = props;
+    const { onChange, min, value } = props;
 
     return (
         <input
             type='date'
             min={ min ? min : undefined }
+            value={ value ? value : undefined }
             className={styles.input}
             onChange={(e) => onChange(e.target.value)} />
     )
